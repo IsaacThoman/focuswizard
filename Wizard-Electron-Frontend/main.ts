d721052b43d32f7cfd90b892e2c5f9076c2c247a
Ross <rosshdawson@gmail.com>
2026-02-17 18:43:41 -0500

    Init Electron frontend with HTML, TS, Vite config

---
+++ Wizard-Electron-Frontend/main.ts
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  })
+
+  win.loadURL('http://localhost:5173')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
